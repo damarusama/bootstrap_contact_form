2da94b5694a2f009806b592cd9002970a03c624f
damarusama <gef@ponnuki.net>
2012-11-12 16:49:50 +0100

    Adding the script.js

---
+++ javascripts/script.js
@@ -0,0 +1,56 @@
+// 
+//	jQuery Validate example script
+//	and getQuerystring for error message 
+//	and success message
+//
+//
+
+function getQuerystring(key, default_) {
+        if (default_==null) default_=""; 
+                key = key.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+                var regex = new RegExp("[\\?&]"+key+"=([^&#]*)");
+                var qs = regex.exec(window.location.href);
+        if(qs == null)
+                return default_;
+        else
+                return qs[1];
+};
+
+
+$(document).ready(function(){
+
+	// Validate
+	// http://bassistance.de/jquery-plugins/jquery-plugin-validation/
+	// http://docs.jquery.com/Plugins/Validation/
+	// http://docs.jquery.com/Plugins/Validation/validate#toptions
+	
+		$('#contact-form').validate({
+	    rules: {
+	      name: {
+	        minlength: 2,
+	        required: true
+	      },
+	      email: {
+	        required: true,
+	        email: true
+	      },
+	      subject: {
+	      	minlength: 2,
+	        required: true
+	      },
+	      message: {
+	        minlength: 2,
+	        required: true
+	      }
+	    },
+	    highlight: function(label) {
+	    	$(label).closest('.control-group').addClass('error');
+	    },
+	    success: function(label) {
+	    	label
+	    		.text('OK!').addClass('valid')
+	    		.closest('.control-group').addClass('success');
+	    }
+	  });
+	  
+}); // end document.ready
